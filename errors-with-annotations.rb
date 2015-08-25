25e5a013f85afcb1af4e5e2bc1b227a2637a14d8
Christophe Philemotte <christophe.philemotte@8thcolor.com>
2015-08-25 13:41:36 +0200

    Add a few other errors and annotations

--- errors-with-annotations.rb
+++ errors-with-annotations.rb
@@ -1,5 +1,11 @@
+class WithouhDoc # PULLREVIEW_LATER
+  def set_name(name)
+    @name = name
+  end
+end
+
 def method
   if a = true # PULLREVIEW_WONTFIX
-    put "test assignation"
+    put "test assignation" # PULLREVIEW_FALSE_POSITIVE
   end
 end
